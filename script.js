438cca3d794b197942df78aecc511c8703d3fcd9
cameronallen0 <92368018+cameronallen0@users.noreply.github.com>
2026-06-26 16:11:01 +0100

    Update1

--- script.js
+++ script.js
@@ -6,12 +6,17 @@ const images = [
 let index = 0;
 const slide = document.getElementById("slide");
 
-setInterval(() => {
-  index = (index + 1) % images.length;
+function changeImage() {
+  // fade out
   slide.style.opacity = 0;
 
   setTimeout(() => {
+    index = (index + 1) % images.length;
     slide.src = images[index];
+
+    // fade in
     slide.style.opacity = 1;
-  }, 300);
-}, 3000);
+  }, 800); // must match CSS transition time
+}
+
+setInterval(changeImage, 3000);
